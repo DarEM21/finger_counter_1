9d3e1d0402fe8e0c5ba866ef3c52fad9ad56be41
DarEM21 <darya.yemelyanova.06@gmail.com>
2022-04-11 16:25:06 +0700

    финал(finish)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+import time
 
 cap = cv2.VideoCapture(0) # подключение к камере
 mp_Hands = mp.solutions.hands # хотим распознать руки
@@ -7,10 +8,11 @@ hands = mp_Hands.Hands(max_num_hands = 2) # задаем хар-ки для ра
 mpDraw = mp.solutions.drawing_utils # инцилизация для рисования рук или условия рук...
 
 finger_coord = [(8,6), (12,10), (16,14), (20,18)] # координаты "суставов" четырех пальцев,кроме большого!
-thumb_coord = (4,2) # координаты для большого палеца!
+thumb_coord = (4,3) # координаты для большого палеца!
 
 while cap.isOpened():
     success, image = cap.read()
+    prevTime = time.time()
     if not success:
         print("не удалось получить кадр с web-камеры!")
         continue
@@ -22,22 +24,34 @@ while cap.isOpened():
         for idx, handLms in enumerate(multilandMarks):
             lbl = result.multi_handedness[idx].classification[0].label
             # print(lbl)
-            upCount = 0 # счётчик пальцев
+        upCount = 0 # счётчик пальцев
         for handLms in multilandMarks:
             mpDraw.draw_landmarks(image, handLms,  mp_Hands.HAND_CONNECTIONS)
-            fingerslist = [] # список пальцев
-            for idx, Im in enumerate(handLms.landmark):
+            fingerslist = [] # список ключевых точек в пикселях
+            for Im in handLms.landmark:
                 h, w, c = image.shape
                 cx, cy = int(Im.x * w), (Im.y * h)
                 fingerslist.append((cx, cy))
+            side = "left"
+            if fingerslist[5][0] > fingerslist[17][0]:
+                side = 'right'
+
             for coordinate in finger_coord:
                 if fingerslist[coordinate[0]][1] < fingerslist[coordinate[1]][1]:
                     upCount += 1
-            if fingerslist[thumb_coord[0]][0] < fingerslist[thumb_coord[1]][0]:
-                upCount += 1
-        cv2.putText(image, str(upCount), (50,150), cv2.FONT_HERSHEY_DUPLEX, 12, (0,200,50), 12)    
+            if side == "left":   
+                if fingerslist[thumb_coord[0]][0] < fingerslist[thumb_coord[1]][0]:
+                    upCount += 1
+            else:
+                if fingerslist[thumb_coord[0]][0] > fingerslist[thumb_coord[1]][0]:
+                    upCount += 1
+
+        cv2.putText(image, str(upCount), (50,150), cv2.FONT_HERSHEY_DUPLEX, 5, (0,200,50), 12)    
         print(upCount)
-        
+
+    currentTime = time.time()
+    fps = 1 / (currentTime - prevTime)
+    cv2.putText(image, f"FPS: {int(fps)}",(400, 150), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (240,100,0), 1)    
     cv2.imshow("image", image)
     if cv2.waitKey(1) & 0xFF == 27: # ESC
         break
